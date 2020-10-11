bb2707ed66e66957725e3cd3ce4674baeafe2027
Luis Araujo <luis_araujocaballero@hotmail.com>
2020-10-10 23:30:33 -0600

    tests

--- src/tests/trailers.test.ts
+++ src/tests/trailers.test.ts
@@ -1,17 +1,34 @@
+/**
+ * @jest-environment node
+ */
+
 import { getImdbId } from '../services/TrailerService';
 
 const HollywooodLink = 'https://content.viaplay.se/pc-se/film/once-upon-a-time...-in-hollywood-2019';
 const bohemianRhapsody = 'https://content.viaplay.se/pc-se/film/bohemian-rhapsody-2018';
-
+const jojoRabbit = 'https://content.viaplay.se/pc-se/film/jojo-rabbit-2019';
 
 test('Get Once Upon a time in Hollywood IMDB Id', async () => {
     const imdb = await getImdbId(HollywooodLink);
-    console.log("codigo", imdb);
-    expect(imdb).toEqual('tt2584384');
+    
+    expect(imdb).toEqual('tt7131622');
 });
 
 test('Get Bohemian Rhapsody IMDB Id', async () => {
     const imdb = await getImdbId(bohemianRhapsody);
-    expect(imdb).toBe('tt1727824');
+    expect(imdb).toEqual('tt1727824');
+
+});
+
+test('Get JojoRabbit IMDB Id', async () => {
+    const imdb = await getImdbId(jojoRabbit);
+    expect(imdb).toEqual('tt2584384');
+
+});
+
+test('Get JojoRabbit IMDB Id', async () => {
+    const imdb = await getImdbId(jojoRabbit + '123');
+    console.log("codigo", imdb);
+    expect(imdb).toThrow('Error: Request failed with status code 404');
 
 });
