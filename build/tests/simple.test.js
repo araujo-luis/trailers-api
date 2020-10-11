efd44c0193f82db1a85fd800c2c04cb4988ff3b4
Luis Araujo <luis_araujocaballero@hotmail.com>
2020-10-10 22:30:44 -0600

    Build gitignore

--- build/tests/simple.test.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var func_1 = require("../controllers/func");
-test('adds 1 + 2 to equal 3', function () {
-    expect(func_1.sum(1, 2)).toBe(3);
-});
